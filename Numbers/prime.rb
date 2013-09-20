a9ca9c3ed219d9a9a554a671c24c5982d0ba0fed
Jerroyd Moore <jerroyd.n.moore@intel.com>
2013-09-20 14:05:58 -0700

    shortened prime.rb

--- Numbers/prime.rb
+++ Numbers/prime.rb
@@ -3,25 +3,21 @@ while 1 do
   STDOUT.flush
 
   num = gets.chomp.to_i
-  if num < 1 then
-    break
-  end
+  break if num < 1
 
   factors = [1]
   itor = 2
 
   while itor < num do
     if num % itor == 0 then
-      factors.push itor
+      factors << itor
       num /= itor
     else
       itor += 1
     end
   end
 
-  if num != 1 then
-    factors.push num
-  end
+  factors << num if num != 1
 
   puts "These are the factors"
   puts factors.inspect
